bab19531c1f7ddc7e624f56aeda3176d244ff177
Alicia Wisdom <wisdoma0992@gmail.com>
2023-12-11 15:21:59 +0000

    this adds styling to the labels and formats the date

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,4 +1,6 @@
 import { React, useState } from 'react';
+import dayjs from 'dayjs';
+
 
 const ArticleList = props => {
     return (
@@ -8,16 +10,16 @@ const ArticleList = props => {
                     return (
                         <li className='article-list-item' key={article.article_id}>
                             <h2>{article.title}</h2>
-                            <p>Topic: {article.topic}</p>
+                            <p><span style={{fontWeight: 'bold'}}>Topic: </span>{article.topic}</p>
                             <img
                                 src={article.article_img_url}
                                 alt={article.title}
                             />
                             <p>{article.body}</p>
-                            <p>Author: {article.author}</p>
-                            <p>Created at: {article.created_at}</p>
-                            <p>Votes: {article.votes}</p>
-                            <p>Comment count: {article.comment_count}</p>
+                            <p><span style={{fontWeight: 'bold'}}>Author: </span>{article.author}</p>
+                            <p><span style={{fontWeight: 'bold'}}>Created at: </span>{dayjs(article.created_at).format('MMMM D, YYYY h:mm A')}</p>
+                            <p><span style={{fontWeight: 'bold'}}>Votes: </span>{article.votes}</p>
+                            <p><span style={{fontWeight: 'bold'}}>Comment count: </span>{article.comment_count}</p>
                         </li>
                     );
                 })}
