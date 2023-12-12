b4c7ec6e495e73e54fc1252342540667914909df
Alicia Wisdom <wisdoma0992@gmail.com>
2023-12-12 10:02:55 +0000

    this adds reusable 'bold' styling in css

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -9,24 +9,41 @@ const ArticleList = props => {
         navigate(`/articles/${id}`, { state: { id } });
     };
 
-
     return (
         <div>
-            <ul className='article-list'>
+            <ul className="article-list">
                 {props.articles.map(article => {
                     return (
-                        <li className='article-list-item' key={article.article_id}>
+                        <li
+                            className="article-list-item"
+                            key={article.article_id}
+                        >
                             <h2>{article.title}</h2>
-                            <p><span style={{fontWeight: 'bold'}}>Topic: </span>{article.topic}</p>
+                            <p>
+                                <span className="bold-styling">Topic: </span>
+                                {article.topic}
+                            </p>
                             <img
                                 src={article.article_img_url}
                                 alt={article.title}
                             />
                             <p>{article.body}</p>
-                            <p><span style={{fontWeight: 'bold'}}>Author: </span>{article.author}</p>
-                            <p><span style={{fontWeight: 'bold'}}>Created at: </span>{dayjs(article.created_at).format('MMMM D, YYYY h:mm A')}</p>
-                            <p><span style={{fontWeight: 'bold'}}>Votes: </span>{article.votes}</p>
-                            <p><span style={{fontWeight: 'bold'}}>Comment count: </span>{article.comment_count}</p>
+                            <p>
+                                <span className="bold-styling">Author: </span>
+                                {article.author}
+                            </p>
+                            <p>
+                                <span className="bold-styling">Created at: </span>
+                                {dayjs(article.created_at).format('MMMM D, YYYY h:mm A')}
+                            </p>
+                            <p>
+                                <span className="bold-styling">Votes: </span>
+                                {article.votes}
+                            </p>
+                            <p>
+                                <span className="bold-styling">Comment count: </span>
+                                {article.comment_count}
+                            </p>
                             <button onClick={event => handleClick(event, article.article_id)}>Read more</button>
                         </li>
                     );
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -26,15 +26,26 @@ const Comments = () => {
     return (
         <div>
             <h2>Comments</h2>
-            <ul className='comments-list'>
+            <ul className="comments-list">
                 {comments.length &&
                     comments.map(comment => {
                         return (
-                            <li className='comments-list-item' key={comment.comment_id}>
+                            <li
+                                className="comments-list-item"
+                                key={comment.comment_id}
+                            >
                                 <p>{comment.body}</p>
-                                <p><span style={{fontWeight: 'bold'}} > Author: </span>{comment.author}</p>
-                                <p><span style={{fontWeight: 'bold'}}> Votes: </span>{comment.votes}</p>
-                                <p><span style={{fontSize: '12px'}}>{dayjs(comment.created_at).format('MMMM D, YYYY h:mm A')}</span></p>
+                                <p>
+                                    <span className="bold-styling"> Author: </span>
+                                    {comment.author}
+                                </p>
+                                <p>
+                                    <span className="bold-styling"> Votes: </span>
+                                    {comment.votes}
+                                </p>
+                                <p>
+                                    <span className="bold-styling">{dayjs(comment.created_at).format('MMMM D, YYYY h:mm A')}</span>
+                                </p>
                             </li>
                         );
                     })}
