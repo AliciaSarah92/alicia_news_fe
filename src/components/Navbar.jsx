b6f0709df5dc713e3763c43ebf493d12a4bfecb3
Alicia Wisdom <wisdoma0992@gmail.com>
2023-12-14 18:24:21 +0000

    logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,27 @@ const Navbar = props => {
     };
     const toTopics = () => {
         navigate('/topics');
-    }
-
+    };
+    const handleLogout = () => {
+        localStorage.removeItem('user');
+        props.setLoggedInUser(null);
+        window.alert('You have been logged out');
+    };
     return (
         <nav className="nav">
-            <button className='styled-btn' onClick={toHome}>Home</button>
-            <button className='styled-btn' onClick={toLogin}>Login</button>
-            <button className='styled-btn' onClick={toTopics}>Topics</button>
+            <button
+                className="styled-btn"
+                onClick={toHome}
+            >
+                Home
+            </button>
+            {props.loggedIn ? <button className="styled-btn" onClick={handleLogout}>Logout</button> : <button className="styled-btn" onClick={toLogin}>Login</button>}
+            <button
+                className="styled-btn"
+                onClick={toTopics}
+            >
+                Topics
+            </button>
             <p>Logged in as: {props.loggedIn ? props.loggedIn.username : 'Guest'}</p>
         </nav>
     );
